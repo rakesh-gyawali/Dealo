c7e062bb57acb129d829f12cd6b71c855a26518e
rakesh-gyawali <rakeshgyawali@gmail.com>
2021-08-02 15:07:55 +0545

    welcome screen created

--- App.js
+++ App.js
@@ -6,64 +6,11 @@ import {
   SafeAreaView,
   Platform,
   StatusBar,
+  Image,
+  ImageBackground,
 } from "react-native";
-
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
-  return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "#fff",
-        flexDirection: "row",
-        justifyContent: "center", //main
-        alignItems: "center", //secondary
-        alignContent: "center",
-        flexWrap: "wrap",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-          top: 27,
-          position: "absolute",
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "grey",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "greenyellow",
-          width: 100,
-          height: 100,
-        }}
-      />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
---
+++ Gallery.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  ImageBackground,
+} from "react-native";
+
+export default function Gallery() {
+  return <View></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "black",
+  },
+});
