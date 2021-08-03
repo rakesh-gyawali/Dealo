0955b864d70e1ac4086181ac8e7c29d744142f26
rakesh-gyawali <rakeshgyawali@gmail.com>
2021-08-03 11:53:39 +0545

    built listItem component

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, Platform, StyleSheet } from "react-native";
 //encapsulating the style for other texts ....
 // for this need to pass "children in between the text"
+//style objects are in array to override the style when used in other components ...
 function AppText({ children, style }) {
   return <Text style={[styles.text, style]}>{children}</Text>;
 }
--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Image, View, StyleSheet, Text } from "react-native";
 import AppText from "../components/AppText";
 import colors from "../config/colors";
+import ListItem from "../components/ListItem";
 
 function ListingDetailsScreen(props) {
   return (
@@ -10,6 +11,13 @@ function ListingDetailsScreen(props) {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red jacket for sale</AppText>
         <AppText style={styles.price}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/men.jpg")}
+            title="Rakesh Gyawali"
+            subTitle="5 Listings"
+          />
+        </View>
       </View>
     </View>
   );
@@ -31,6 +39,9 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: "500",
+  },
+  userContainer: {
+    marginVertical: 40,
   },
 });
 
