6d30e5c84cdb7528b573e5955645423a2dd2375f
rakesh-gyawali <rakeshgyawali@gmail.com>
2021-08-03 09:02:59 +0545

    building button component

--- App.js
+++ App.js
@@ -11,14 +11,14 @@ import {
 } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
-import AppText from "./app/comoponents/AppText";
+import AppText from "./app/components/AppText";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
   return (
-    <View>
-      <MaterialCommunityIcons name="email" size={200} />
-      <AppText>Hello this is AppText</AppText>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <AppButton title="Login" onPress={() => console.log("Tapped")} />
     </View>
   );
 }
