01fee6d29e3f3de1d115117597f1e3c5d59c4a8b
rakesh-gyawali <rakeshgyawali@gmail.com>
2021-08-03 11:57:38 +0545

    added icons in imageView screen

--- App.js
+++ App.js
@@ -17,5 +17,5 @@ import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
