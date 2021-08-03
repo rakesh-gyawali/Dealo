0795521adbb401b38dcaf90c1b9c60289256cf5d
rakesh-gyawali <rakeshgyawali@gmail.com>
2021-08-03 10:59:08 +0545

    building card component

--- App.js
+++ App.js
@@ -14,7 +14,21 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppText from "./app/components/AppText";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import AppButton from "./app/components/AppButton";
-
+import Card from "./app/components/Card";
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <View
+      style={{
+        backgroundColor: "#f8f4f4",
+        padding: 20,
+        paddingTop: 100,
+      }}
+    >
+      <Card
+        title="Red jacket for sale"
+        subTitle="$100"
+        image={require("./app/assets/jacket.jpg")}
+      />
+    </View>
+  );
 }
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Text, Platform, StyleSheet } from "react-native";
 //encapsulating the style for other texts ....
 // for this need to pass "children in between the text"
-function AppText({ children }) {
-  return <Text style={styles.text}> {children}</Text>;
+function AppText({ children, style }) {
+  return <Text style={[styles.text, style]}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
